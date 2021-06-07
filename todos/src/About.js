025e3d99d619ecb3cdd2c818a97fcb20bea0f882
huangkairan <18757128360@163.com>
2021-06-07 14:53:34 +0800

    feat: 子应用添加路由

---
+++ todos/src/About.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function About() {
+  return <div>About component</div>;
+}
+export default About;
--- todos/src/root.component.js
+++ todos/src/root.component.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Redirect, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
 export default function Root(props) {
+  // return (
+  //   <div>
+  //     <section>{props.name} is mounted!</section>
+  //     <div>React子应用</div>
+  //   </div>
+  // );
   return (
-    <div>
-      <section>{props.name} is mounted!</section>
-      <div>React子应用</div>
-    </div>
+    <BrowserRouter basename="/todos">
+      <div>{props.name}</div>
+      <div>
+        <Link to="/home">Home</Link>
+        <span> | </span>
+        <Link to="/about">About</Link>
+      </div>
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Redirect to="home" />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
