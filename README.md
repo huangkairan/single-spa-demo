463d72da425fcf160ddc69f32d16668f4884fe5e
huangkairan <18757128360@163.com>
2021-06-07 12:26:59 +0800

    feat: initial container

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # single-spa-demo
+
 single-spa-demo for practice
+
+### single-spa 中三种类型的微前端应用
+
+1. single-spa-application/parcel
+
+- 微前端架构中的微应用，可以用 vue、react、angular 等
+
+2. single-spa root config
+
+- 创建微前端容器应用
+
+3. utility modules
+
+- 公共模块应用，非渲染组件，用于跨应用共享 js 逻辑的微应用
--- container/src/study-root-config.js
+++ container/src/study-root-config.js
@@ -1,5 +1,11 @@
 import { registerApplication, start } from "single-spa";
 
+/**
+ * 注册应用
+ *  name string 应用名称： @组织名称/应用名称
+ *  app fn 通过systemjs引用打包好的微前端应用模块代码umd 返回promise
+ *  activeMenu array 路由匹配时激活应用
+ */
 registerApplication({
   name: "@single-spa/welcome",
   app: () =>
@@ -16,5 +22,10 @@ registerApplication({
 // });
 
 start({
+  /**
+   * urlRerouteOnly
+   * 是否可以通过history.pushState()和history.replaceState()更改触发single-spa路由
+   * true不允许，false允许
+   */
   urlRerouteOnly: true,
 });
